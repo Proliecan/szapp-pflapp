032de37e1fb8b1e44d94d6fddd6a31d51b60a3b2
DE38 <niklas@gt-trenz.de>
2022-06-23 12:56:05 +0200

    additional changes Additional changes and merges/deletions of old code base. Project is nor orderly structured

--- server/routes.py
+++ server/routes.py
@@ -1,20 +1,21 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from server.db_models import Plant, WaterlevelData
 from server import app
 
 # -----------------------------------------
 # routes
-# create basic route
+
+# redirect to start-page 
 @app.route('/')
 def home():
+    return redirect(url_for('start'), code=302)
+
+# start-page
+@app.route('/start')
+def start():
     return render_template('index.html')
 
-@app.route('/startseite')
-def start():
-    return render_template('startseite.html')
-
-
-# route to imprint
+# imprint
 @app.route('/imprint')
 def imprint():
     return render_template('imprint.html')
