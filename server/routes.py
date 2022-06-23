41f63b24e61fec3a424093c407a46a81fd754c93
DE38 <niklas@gt-trenz.de>
2022-06-23 21:35:14 +0200

    SP-36 add new route

--- server/routes.py
+++ server/routes.py
@@ -11,11 +11,15 @@ def home():
     return redirect(url_for('start'), code=302)
 
 # start-page
-@app.route('/start')
+@app.route('/start', methods=['GET'])
 def start():
     return render_template('index.html')
 
 # imprint
-@app.route('/imprint')
+@app.route('/imprint', methods=['GET'])
 def imprint():
     return render_template('imprint.html')
+
+@app.route('/main_page', methods=['GET'])
+def main_page():
+    return render_template('main_page.html')
